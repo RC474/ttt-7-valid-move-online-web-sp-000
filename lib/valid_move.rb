b8cac293b318aa0080ccb34d1b5e0dba90d5f474
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-11-22 02:37:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index)
     return true
-    elsif !position_taken?(board, index)
+    elsif !=position_taken?(board, index)
     return false 
     elsif index.between?(0, 8)
     return true 
